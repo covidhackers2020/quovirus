2a24b8df36e001d8160f139faa31f5562d7326f1
Mel Padden <mel.padden@outlook.com>
2020-03-29 18:23:05 +0200

    Update takemehome.py

--- takemehome.py
+++ takemehome.py
@@ -1,10 +1,14 @@
-import requests
+import requests,json
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0"
+url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO-sky/ORD-sky/2020-03-30"
 
-payload = "inboundDate=2020-30-30&cabinClass=any&children=0&infants=0&country=US&currency=USD&locale=en-US&originPlace=SFO-sky&destinationPlace=LHR-sky&outboundDate=2019-03-30-01&adults=1"
+querystring = {"inboundpartialdate":"2020-03-30"}
+
 headers = {
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    'x-rapidapi-key': "7cd9631a1bmsh29e6da19e03b95fp1d6c8cjsnd456db97937b",
-    'content-type': "application/x-www-form-urlencoded"
+    'x-rapidapi-key': "7cd9631a1bmsh29e6da19e03b95fp1d6c8cjsnd456db97937b"
     }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(json.dumps(response.text, indent=4, sort_keys=True))
